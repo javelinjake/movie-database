63f38f942c18457dc797e3f7dd59a352788cdb96
Jacob Lewis <jake@jakepeterlewis.com>
2015-05-28 00:15:19 +1000

    A lot of changes
    Added filter list style, added clickable movies, added movie layer,
    added movie content

--- js/main.js
+++ js/main.js
@@ -10,15 +10,38 @@ app.controller('myController', function($scope, $http) {
 	var yyyy = today.getFullYear();
 	if(dd<10) { dd='0'+dd } if(mm<10) { mm='0'+mm } today = yyyy+'-'+mm+'-'+dd;
 	
-	$http.get('https://api.themoviedb.org/3/discover/movie?api_key=' + apiKey + '&release_date.lte='+ today +'&vote_average.gte=8&vote_count.gte=20&sort_by=vote_average.desc').
-	success(function(data, status, headers, config) {
-		// this callback will be called asynchronously
-		// when the response is available
-		$scope.data = data;
-	}).
-	error(function(data, status, headers, config) {
-		// called asynchronously if an error occurs
-		// or server returns response with an error status.
-	});
+	$scope.filter1 = '&release_date.lte='+ today +'&vote_average.gte=8&vote_count.gte=20&sort_by=vote_average.desc';
+	$scope.filter2 = '&release_date.lte='+ today +'&vote_average.lte=5&vote_count.gte=20&sort_by=vote_average.asc';
+	$scope.filter3 = '&release_date.gte='+ today +'&vote_count.gte=20&sort_by=vote_average.desc';
+	$scope.filter4 = '&release_date.lte='+ today +'&vote_average.gte=7&popularity.desc';
+	
+	$scope.moviePageTrue = false;
+	
+	$scope.filters = function(filter) {
+		$http.get('https://api.themoviedb.org/3/discover/movie?api_key=' + apiKey + filter).
+		success(function(data, status, headers, config) {
+			$scope.movies = data;
+		}).
+		error(function(data, status, headers, config) {
+			
+		});
+	};
+	
+	$scope.moviePage = function(movieId) {
+		$http.get('https://api.themoviedb.org/3/movie/'+ movieId +'?api_key=' + apiKey).
+		success(function(data, status, headers, config) {
+			$scope.movieData = data;
+		}).
+		error(function(data, status, headers, config) {
+			
+		});
+		
+		$scope.moviePageTrue = true;
+		window.scrollTo(0, 0);
+	}
+	
+	$scope.hideMoviePage = function() {
+		$scope.moviePageTrue = false;
+	}
 	
 });
